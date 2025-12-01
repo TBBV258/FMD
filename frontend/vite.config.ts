ced65b3362c9b0350ea7b956ab6bd738d53fda23
TBBV258 <vanpaulocossa@gmail.com>
2025-12-02 00:46:59 +0200

    Update Vite configuration to set base path for GitHub Pages deployment.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
   // Uncomment the line below and set repository name when deploying to GitHub Pages
-  // base: '/your-repository-name/',
+  base: '/FMD/',
   plugins: [
     vue(),
     VitePWA({
